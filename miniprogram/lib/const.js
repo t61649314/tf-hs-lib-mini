3a627dd04bd0f36b456b6c90a843a216b3801cb6
田放 <459126479@qq.com>
2020-04-01 20:42:28 +0800

    广告审核不通过

--- miniprogram/lib/const.js
+++ miniprogram/lib/const.js
@@ -1,48 +1,43 @@
 const occupationInfo = {
   Druid: {
     cnName: "德鲁伊",
-    simpleName: "德",
-    dbfId: 274
+    simpleName: "德"
   },
   Hunter: {
     cnName: "猎人",
     simpleName: "猎",
-    dbfId: 31
   },
   Mage: {
     cnName: "法师",
     simpleName: "法",
-    dbfId: 637
   },
   Paladin: {
     cnName: "骑士",
     simpleName: "骑",
-    dbfId: 671
   },
   Priest: {
     cnName: "牧师",
     simpleName: "牧",
-    dbfId: 813
   },
   Rogue: {
     cnName: "盗贼",
     simpleName: "贼",
-    dbfId: 930
   },
   Shaman: {
     cnName: "萨满",
     simpleName: "萨",
-    dbfId: 1066
   },
   Warlock: {
     cnName: "术士",
     simpleName: "术",
-    dbfId: 893
   },
   Warrior: {
     cnName: "战士",
     simpleName: "战",
-    dbfId: 7
+  },
+  "Demon Hunter": {
+    cnName: "恶魔猎手",
+    simpleName: "瞎",
   },
 };
 
